59af382c9efa093adf2ec71833d777d3fc7081e6
devaliy <indexcorpng@gmail.com>
2023-04-26 17:25:27 +0100

    new changes

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+
+<!-- Jquery Core Js --> 
+<script src="assets/bundles/libscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js ( jquery.v3.2.1, Bootstrap4 js) --> 
+<script src="assets/bundles/vendorscripts.bundle.js"></script> <!-- slimscroll, waves Scripts Plugin Js -->
+
+<script src="assets/bundles/morrisscripts.bundle.js"></script><!-- Morris Plugin Js -->
+<script src="assets/bundles/jvectormap.bundle.js"></script> <!-- JVectorMap Plugin Js -->
+<script src="assets/bundles/knob.bundle.js"></script> <!-- Jquery Knob Plugin Js -->
+<script src="assets/bundles/countTo.bundle.js"></script> <!-- Jquery CountTo Plugin Js -->
+<script src="assets/bundles/sparkline.bundle.js"></script> <!-- Sparkline Plugin Js -->
+
+<script src="assets/bundles/mainscripts.bundle.js"></script>
+<script src="assets/js/pages/index.js"></script>
+</body>
+
+<!-- Mirrored from www.wrraptheme.com/templates/compass/estate/index by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 31 Dec 2022 10:07:18 GMT -->
+</html>
